7a697fd09a8e00635909f2482ecac89a33fdb8b9
Sagar JP <jpsagarm95@gmail.com>
2022-03-11 12:56:07 -0800

    Loader changes

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,12 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
+import Loader from '../components/Loader';
+
 export default function Home() {
   return (
     <div>
-      <Link prefetch={true} href={{
-        pathname: '/[username]',
-        query: {username: 'jpsagar'}
-      }}>Sagar's Profile</Link>
+      <Loader show/>
     </div>
-  )
+  );
 }
